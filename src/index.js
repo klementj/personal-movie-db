b36809acacc3d5089000245d95811af23d25d988
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-12-11 10:55:30 +0100

    moviecard.js modul som render HTML ud fra Object

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import 'babel-polyfill'
 // Import JS files
 import 'materialize-css'
 
+
+
+import movieCard from './js/view/movieCard.js'
+
+let movieObj = {
+  title: 'repilicus',
+  year: 1997,
+  genre: 'romance',
+}
+
+console.log(new movieCard(movieObj).render())
