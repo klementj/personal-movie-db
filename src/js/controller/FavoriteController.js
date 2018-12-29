cb8420d76fb0339950d7ab15bc11639d3b450829
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-12-29 10:35:15 +0100

    fav controller and view before restructuring

--- src/js/controller/FavoriteController.js
+++ src/js/controller/FavoriteController.js
@@ -1,13 +1,66 @@
-import FavoriteView from '../view/FavoriteView';
+import FavoriteView from '../view/FavoriteView'
 
 export default class FavoriteController{
 
-  setupView(){
-    let HTMlelement = document.getElementById('favoriteSection')
-    let movieCardHTML = new FavoriteView().populateWithMovies()
-    console.log(movieCardHTML)
-    
-    HTMlelement.insertAdjacentHTML('beforeend', movieCardHTML)
+  /**
+   * Load from store (Maybe pass store to favoriteview so no need to load store in favoriteView)
+   * Call createView in FavoriteView (move html to favoriteview)
+   * Setup (or call?) event listeners
+   * ONLY CALL THIS SETUP
+   */
+  static setupView(){
+    const movieCardHTML = new FavoriteView().populateWithMovies()
+    const favoriteHTML = `
+      <!-- Favorite Movies -->
+      <div class="container">
+        <h2>Favorite Movies</h2>
+      </div>
+
+      <main>       
+        <section class="col">  
+          <div class="grey darken-4" id="sec">
+            <div class="container">
+              <div class="row" id="favoriteSection">
+                ${movieCardHTML}
+              </div>
+            </div>
+          </div>
+        </section>
+    `
+    document.body.insertAdjacentHTML('beforeend', favoriteHTML)
+  }
+
+  static setupEventListeners(){
+    const favoriteSection = document.getElementById('favoriteSection')
+
+    favoriteSection.addEventListener('click', (e) => {
+      const clickedElement = e.target;
+      // console.log(clickedElement)
+
+      if (clickedElement.matches('a.favorite-movie-button')) {
+        const movieID = clickedElement.getAttribute('data-id')
+        
+        // removeParrentArticle from DOMError
+        clickedElement.closest('article').remove()
+        // clickedElement.parentElement.parentElement.parentElement.parentElement.remove()
+        console.log(`Removed movie with imdbID ${movieID} from favorite section`)
+
+        // remove movie from Store
+
+      }
+    })
+  }
+
+  static addMovie(){
+    const favoriteSection = document.getElementById('favoriteSection')
+    // Create new movie card component and append
+    document.body.insertAdjacentHTML('beforeend', favoriteSection)
+
+
+    /**
+     * Call add movie in favoriteView (everything that is seen)
+     * Call and change the store
+     */
   }
 
 }
--- src/js/view/FavoriteView.js
+++ src/js/view/FavoriteView.js
@@ -1,14 +1,26 @@
-import MovieCardComponent from './MovieCardComponent';
-import {store} from '../model/Store';
+import MovieCardComponent from './MovieCardComponent'
+import { store } from '../model/Store-OLD'
 
 export default class FavoriteView{
+// Constructor kan eventuelt kalde en setupView metode.  Så der kun skal laves en ny FavoritView instans.
+// Giver også mening i forhold til navngivningen og hvad den så gør. 
+// Giver mindre mening ift. at filen bliver større og ikke i samme grad efterligner et moderne Front-end webapp framework.
+
 
   populateWithMovies(){
+
     let moviesString = ''
-    store.forEach(movieObject => {
-      moviesString += new MovieCardComponent(movieObject).render()
-    })
-    return moviesString
+      
+    console.log(store.movieDB)
+
+    if (store) {
+      store.movieDB.forEach(movieObject => {
+        moviesString += new MovieCardComponent(movieObject).render()
+      })
+      return moviesString
+    }
+    
+    console.error("MovieObject doesn't exist", store)  
   }
 
 }
