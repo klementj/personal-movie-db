52befd0bc2803cf6c0dacd38e5ff4780e437039c
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-12-29 10:31:22 +0100

    Importing App in entry file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-import './scss/styles.scss'
 // import babel-polyfill for async function declearation and await operator
 import 'babel-polyfill'
+
+// Import stylesheet
+import './scss/styles.scss'
 // Import JS files
-
-
-// TEST til debug
-import FavoriteController from './js/controller/FavoriteController'
-
-new FavoriteController().setupView()
-
-// let testMovie = {
-//   title: 'test',
-//   year: 1900,
-// }
-
-// import movieCard from './js/view/MovieCardComponent'
-
-// let oneCard = new movieCard(testMovie).render()
-
-// console.log(oneCard)
-
-// import { store } from './js/model/Store'
-// console.log(store)
-
-// import FavoriteView from './js/view/FavoriteView'
-
-// let bar = new FavoriteView().populateWithMovies()
-// console.log(bar)
-
-// APP START
-// import favoriteController from './js/controller/FavoriteController'
-
-
+import 'materialize-css'
+import './js/App'
